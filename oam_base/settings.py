8a3f1143337225f6bf1556d536817e85391573ab
Harley Hauer <hhauer@pdx.edu>
2014-05-08 11:13:59 -0700

    Updated location of static files.

--- oam_base/settings.py
+++ oam_base/settings.py
@@ -52,7 +52,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/var/www/oam_static/'
+STATIC_ROOT = '/var/www/html/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
