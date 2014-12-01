3d04e0a74911a111834ecd97e91cfd009647cd17
Harley Hauer <hhauer@pdx.edu>
2014-12-01 14:51:06 -0800

    Missed a character in the regex for password reset urls.

--- PasswordReset/urls.py
+++ PasswordReset/urls.py
@@ -4,7 +4,7 @@ from PasswordReset import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'reset/(?P<token>[:\w]+)/$', views.reset, name='reset'),
+    url(r'reset/(?P<token>[-:\w]+)/$', views.reset, name='reset'),
     url(r'reset/$', views.reset, name='reset_notoken'),
     #url(r'AUP/$', views.AUP, name='AUP'),
 )
