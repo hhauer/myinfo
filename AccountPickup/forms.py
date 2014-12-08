5aa0700cff316f55c47ec1fb7ca3dabd8436fcd6
Justin McClure <justin8@pdx.edu>
2014-12-08 09:41:51 -0800

    Change 'Password' to 'Activation Code', to match explanatory text and  clarify what input is needed. Fix checkbox placement to account for field text change.

--- AccountPickup/forms.py
+++ AccountPickup/forms.py
@@ -1,23 +1,26 @@
 from django import forms
-from MyInfo.forms import ContactInformationForm
 
 import logging
 logger = logging.getLogger(__name__)
 
+
 class AccountClaimLoginForm(forms.Form):
     id_number = forms.CharField(min_length=9, max_length=9, label="ID Number")
     birth_date = forms.DateField(label="Birth Date (MM/DD/YYYY)")
-    auth_pass = forms.CharField(label="Password", widget=forms.PasswordInput())
-    
+    auth_pass = forms.CharField(label="Activation Code", widget=forms.PasswordInput())
+
+
 class AcceptAUPForm(forms.Form):
     accepted = forms.BooleanField(label="I Accept")
+
 
 # Must override __init__ to dynamically allocate choices for each user.
 class OdinNameForm(forms.Form):
     def __init__(self, names, *args, **kwargs):
         super(OdinNameForm, self).__init__(*args, **kwargs)
         self.fields["name"] = forms.ChoiceField(choices=names, label="Odin Username")
-                
+
+
 class EmailAliasForm(forms.Form):
     def __init__(self, names, *args, **kwargs):
         super(EmailAliasForm, self).__init__(*args, **kwargs)
