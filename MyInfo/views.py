be7263fa2efa86b3b361b0fcf2933d49ca3b4949
Justin McClure <justin8@pdx.edu>
2015-01-16 09:39:49 -0800

    Add comment note about unused code

--- MyInfo/views.py
+++ MyInfo/views.py
@@ -85,7 +85,7 @@ def set_password(request):
     else:
         form = formNewPassword(request.POST or None)
 
-    if 'identity' not in request.session:
+    if 'identity' not in request.session:  # Shouldn't happen, the above get_or_create would error out first.
         logger.critical("service=myinfo error=\"No identity information available at set password. Aborting. \
                 \" session=\"{0}\"".format(request.session))
         return HttpResponseServerError('No identity information was available.')
