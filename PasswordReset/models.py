5f960ecd85cfe20ec687372d52f6e7e4375fe9b3
Justin McClure <justin8@pdx.edu>
2015-02-05 16:22:24 -0800

    Tweaked allowed characters for the reset token. Tokens were still using 0 and 1, which can look like O and I. Also was excluding capital L and capital U, which aren't ambiguous.

--- PasswordReset/models.py
+++ PasswordReset/models.py
@@ -5,28 +5,29 @@ from django.utils import timezone
 
 # http://www.crockford.com/wrmg/base32.html
 # Should reduce confusion on characters when typing in from a phone. No I/1 l/I l/1 confusion.
-CHARSET = '0123456789ABCDEFGHJKMNPQRSTVWXYZ'
+CHARSET = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ'
 LENGTH = 10
+
 
 # This model is based largely on the model available at:
 # https://github.com/workmajj/django-unique-random/
 class TextMessageShortCode(models.Model):
-    code = models.CharField(max_length = LENGTH)
-    token = models.CharField(max_length = 256)
+    code = models.CharField(max_length=LENGTH)
+    token = models.CharField(max_length=256)
     
     created = models.DateTimeField(auto_now_add=True)
     
     def save(self, *args, **kwargs):
         
         # Delete old tokens first.
-        TextMessageShortCode.objects.filter(created__lte = timezone.now() - timedelta(days=7)).delete()
+        TextMessageShortCode.objects.filter(created__lte=timezone.now()-timedelta(days=7)).delete()
         
         unique = False
         while not unique:
             new_code = ''
             for _ in range(LENGTH):
                 new_code += CHARSET[randrange(0, len(CHARSET))]
-            if not TextMessageShortCode.objects.filter(code = new_code):
+            if not TextMessageShortCode.objects.filter(code=new_code):
                 self.code = new_code
                 unique = True
         
