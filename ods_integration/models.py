22c5f596c265465c6592759498c901bbda088ad6
Harley Hauer <hhauer@pdx.edu>
2015-02-12 16:47:42 -0800

    Added initial event model for ODS integration. Removed views.py from ODS integration as it does not have any front-end components.

--- ods_integration/models.py
+++ ods_integration/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Event(models.Model):
+    EVENT_TYPES = (
+        ('AUP Accepted', 'AUP Accepted'),
+    )
+
+    psu_uuid = models.CharField(max_length=36)
+    spriden_id = models.CharField(max_length=32)
+    event_type = models.CharField(max_length=32, choices=EVENT_TYPES)
+    event_date = models.DateField()
+
+    def __str__(self):
+        return "[{}] for {}".format(self.event_type, self.psu_uuid)
