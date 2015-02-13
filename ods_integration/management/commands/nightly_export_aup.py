658219150a6b8b712ad31d63b63f6e2bdf03aa4f
Harley Hauer <hhauer@pdx.edu>
2015-02-12 17:23:00 -0800

    Refactored export AUP to log events to the ods integration table instead of a CSV.

--- ods_integration/management/commands/nightly_export_aup.py
+++ ods_integration/management/commands/nightly_export_aup.py
@@ -3,12 +3,12 @@ import requests
 __author__ = 'hhauer'
 
 import datetime
-import csv
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from AccountPickup.models import OAMStatusTracker
+from ods_integration.models import Event
 
 
 class Command(BaseCommand):
@@ -20,28 +20,31 @@ class Command(BaseCommand):
         # For making the logs easier to interpret later, log out when we started.
         self.stdout.write("Nightly Export AUP started at: {}".format(datetime.datetime.now()))
 
-        yesterday = datetime.date.today() - datetime.timedelta(days=1)
-
-        # Come up with a reasonable filename.
-        csv_filename = "aup_acceptance_{}.csv".format(yesterday.strftime("%Y%m%d"))
+        # Truncate old data.
+        self.stdout.write("Truncating previous data, if any.");
+        Event.objects.all().delete()
 
         # Get any oamstatustracker that updated their AUP yesterday.
+        yesterday = datetime.date.today() - datetime.timedelta(days=1)
         aup_changes = OAMStatusTracker.objects.filter(agree_aup=yesterday)
 
-        with open(csv_filename, 'w') as file:
-            csv_file = csv.writer(file, delimiter='|')
+        for x in aup_changes:
+            r = requests.get(
+                self.get_iiq_url(x.psu_uuid),
+                auth=(settings.SAILPOINT_USERNAME, settings.SAILPOINT_PASSWORD),
+                verify=False
+            )
 
-            for x in aup_changes:
-                r = requests.get(
-                    self.get_iiq_url(x.psu_uuid),
-                    auth=(settings.SAILPOINT_USERNAME, settings.SAILPOINT_PASSWORD),
-                    verify=False
-                )
+            spriden_id = r.json()
 
-                spriden_id = r.json()
-                row = [spriden_id, 'AUP Accepted', yesterday.strftime("%Y%m%d")]
-                csv_file.writerow(row)
-
+            if spriden_id is None or spriden_id == "":
+                self.stdout.write("Error converting {} to spriden_id.".format(x.psu_uuid))
+            else:
+                Event.objects.create(psu_uuid=x.psu_uuid,
+                                     spriden_id=spriden_id,
+                                     event_type="AUP Accepted",
+                                     event_date=yesterday)
+                self.stdout.write("Created ODS event for " + x.psu_uuid)
 
         # Finally, print out when we finished.
         self.stdout.write("Nightly Export AUP finished at: {}".format(datetime.datetime.now()))
