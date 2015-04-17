0fb136a113cfe429f9577a454dc37088675d492e
Harley Hauer <hhauer@pdx.edu>
2015-04-16 19:03:48 -0700

    Added API call for Duo pre-provisioning. Modified Duo view to use it.

--- Duo/views.py
+++ Duo/views.py
@@ -1,5 +1,5 @@
 import logging
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse_lazy, reverse
 from django.http import HttpResponseRedirect
 
 from django.shortcuts import render
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 from Duo import duo_web
-from lib.api_calls import APIException
+from lib.api_calls import provision_duo
 
 logger = logging.getLogger(__name__)
 
@@ -15,19 +15,32 @@ logger = logging.getLogger(__name__)
 # Duo Login
 @login_required(login_url=reverse_lazy('duoLogin'))
 def login(request):
-    if 'identity' not in request.session or 'ODIN_NAME' not in request.session['identity']:
-        # Error state
-        raise APIException("Attempted to go to Duo Login without a valid identity dictionary of ODIN_NAME dictionary value")
+    # Interaction with Duo occurs entirely over JavaScript, but leverages a widget designed for login.
+    # If the user selects "login" they will post back to this page, but we don't actually want to do anything but
+    # re-draw the widget.
 
-    # TODO: In production username will be PSU_UUID, so we need to do some work to get the odin username
-    if request.method == 'GET':
-        sig_request = duo_web.sign_request(settings.DUO_IKEY, settings.DUO_SKEY, settings.DUO_AKEY, request.session['identity']['ODIN_NAME'])
-        return render(request, 'login.html', {
-            'sig_request': sig_request,
-            'host': settings.DUO_HOST
-        }
-                      )
-    elif request.method == 'POST':
-        sig_response = request.POST.get('sig_response', '')
-        duo_web.verify_response(settings.DUO_IKEY, settings.DUO_SKEY, settings.DUO_AKEY, sig_response)
-        return HttpResponseRedirect(request.path)
+    # Duo API returns a dict.
+    duo = provision_duo(request.user.username)
+
+    if duo['status'] == 'error':
+        if duo['reason'] == 'noidentity':
+            raise NoIdentityException("No matching identity found for user currently logged into MyInfo.")
+        elif duo['reason'] == 'notemployee':
+            logger.error("Non-employee user attempting to access Duo self-registration. " + request.user.username)
+            return HttpResponseRedirect(reverse('AccountPickup:next_step'))
+        else:
+            raise DuoSecurityException("Duo returned an exception: " + duo['message'])
+
+    sig_request = duo_web.sign_request(settings.DUO_IKEY, settings.DUO_SKEY, settings.DUO_AKEY, request.user.username)
+    return render(request, 'login.html', {
+        'sig_request': sig_request,
+        'host': settings.DUO_HOST
+    })
+
+
+class NoIdentityException(Exception):
+    pass
+
+
+class DuoSecurityException(Exception):
+    pass
