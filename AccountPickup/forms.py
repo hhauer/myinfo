206ae80358a43f7a4398809ebf37b9c59156ba63
Justin McClure <justin8@pdx.edu>
2015-04-29 15:15:28 -0700

    Shifted alias setting logic to form level.
    API call
    Info logging
    Session update
    Status tracker update

--- AccountPickup/forms.py
+++ AccountPickup/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from lib.api_calls import set_email_alias, APIException
+from AccountPickup.models import OAMStatusTracker
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -22,6 +25,30 @@ class OdinNameForm(forms.Form):
 
 
 class EmailAliasForm(forms.Form):
-    def __init__(self, names, *args, **kwargs):
+    def __init__(self, session, *args, **kwargs):
         super(EmailAliasForm, self).__init__(*args, **kwargs)
-        self.fields["alias"] = forms.ChoiceField(choices=names, label="Email Alias")
+        self.fields["alias"] = forms.ChoiceField(
+            choices=enumerate(session['TRUENAME_EMAILS']), label="Email Alias")
+        self.session = session
+
+    def save(self):
+        alias = self.session['TRUENAME_EMAILS'][int(self.cleaned_data['alias'])]
+        logger.debug("Email alias value: " + alias)
+
+        if alias is not None and alias != 'None':
+            result = set_email_alias(self.session['identity'], alias)
+            if result is not True:
+                # API call failed
+                raise APIException("API call to set_email_alias did not return success.")
+
+            self.session['identity']['EMAIL_ALIAS'] = alias
+            self.session.modified = True  # Manually notify Django we modified a sub-object of the session.
+
+            logger.info("service=myinfo psu_uuid={0} email_alias={1}".format(
+                self.session['identity']['PSU_UUID'], alias))
+
+        (oam_status, _) = OAMStatusTracker.objects.get_or_create(psu_uuid=self.session['identity']['PSU_UUID'])
+        oam_status.select_email_alias = True
+        oam_status.save(update_fields=['select_email_alias'])
+
+        return self.session
--- AccountPickup/tests/test_forms.py
+++ AccountPickup/tests/test_forms.py
@@ -1,6 +1,14 @@
 __author__ = "Justin McClure"
+from importlib import import_module
+
 from django.test import TestCase
+from django.conf import settings
+
 from AccountPickup.forms import OdinNameForm, EmailAliasForm
+from AccountPickup.models import OAMStatusTracker
+
+
+SessionStore = import_module(settings.SESSION_ENGINE).SessionStore
 
 
 class OdinNameFormTestCase(TestCase):
@@ -28,19 +36,25 @@ class OdinNameFormTestCase(TestCase):
 
 
 class EmailAliasFormTestCase(TestCase):
-    choices = ["Name 1 - Test1",
+    choices = ["None",
+               "Name 1 - Test1",
                "Name 2 - Test2", ]
+    session = SessionStore()
+    session['TRUENAME_EMAILS'] = choices
+    session['identity'] = {'PSU_UUID': '123456789'}
+    session.modified = True
+    session.save()
 
     def test_good_init(self):
         # Test successful init without data
-        form = EmailAliasForm(enumerate(self.choices))
+        form = EmailAliasForm(self.session)
         self.assertEqual(form.fields["alias"].label, "Email Alias")
-        self.assertEqual([c for c in form.fields["alias"].choices], [(0, "Name 1 - Test1"), (1, "Name 2 - Test2")])
+        self.assertEqual([c for c in form.fields["alias"].choices], [
+            (0, "None"), (1, "Name 1 - Test1"), (2, "Name 2 - Test2")])
 
         # Test init with data
-        form = EmailAliasForm(enumerate(self.choices), {"alias": 1})
+        form = EmailAliasForm(self.session, {"alias": 1})
         self.assertEqual(form.fields["alias"].label, "Email Alias")
-        self.assertEqual([c for c in form.fields["alias"].choices], [(0, "Name 1 - Test1"), (1, "Name 2 - Test2")])
         self.assertTrue(form.is_valid())
         self.assertEqual(form.cleaned_data, {"alias": "1"})
 
@@ -49,3 +63,23 @@ class EmailAliasFormTestCase(TestCase):
         self.assertRaises(TypeError, EmailAliasForm)
         # Test init with non-iterable choices
         self.assertRaises(TypeError, EmailAliasForm, 42)
+
+    def test_save(self):
+
+        (oam_status, _) = OAMStatusTracker.objects.get_or_create(psu_uuid=self.session['identity']['PSU_UUID'])
+        self.assertFalse(oam_status.select_email_alias)
+        self.assertNotIn('EMAIL_ALIAS', self.session['identity'])
+        form = EmailAliasForm(self.session, {"alias": 0})
+        self.assertTrue(form.is_valid())
+        form.save()
+        oam_status.refresh_from_db()
+        self.assertTrue(oam_status.select_email_alias)
+        self.assertNotIn('EMAIL_ALIAS', self.session['identity'])
+        oam_status.select_email_alias = False
+        oam_status.save()
+        form = EmailAliasForm(self.session, {"alias": 1})
+        self.assertTrue(form.is_valid())
+        form.save()
+        oam_status.refresh_from_db()
+        self.assertTrue(oam_status.select_email_alias)
+        self.assertIn('EMAIL_ALIAS', self.session['identity'])
