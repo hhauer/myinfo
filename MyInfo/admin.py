6a04f7917cb800fa06b03f6e20ba9c4d3cc7e15c
Justin McClure <justin8@pdx.edu>
2015-05-13 13:44:15 -0700

    Improve model admin searching.
    * Added search by psu_uuid to ContactInformation and DirectoryInformation models.
    * Added display of fields to admin list/search page for these models.

--- MyInfo/admin.py
+++ MyInfo/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from MyInfo.models import Department, Mailcode, Building, DirectoryInformation, ContactInformation, MaintenanceNotice
 
+
+class DirectoryInformationAdmin(admin.ModelAdmin):
+    search_fields = ['^psu_uuid']
+    list_display = ['psu_uuid', 'company', 'telephone', 'fax', 'job_title', 'department', 'office_building',
+                    'office_room', 'mail_code', ]
+
+
+class ContactInformationAdmin(admin.ModelAdmin):
+    search_fields = ['^psu_uuid']
+    list_display = ['psu_uuid', 'cell_phone', 'alternate_email', ]
+
 admin.site.register(Department)
 admin.site.register(Mailcode)
 admin.site.register(Building)
-admin.site.register(DirectoryInformation)
-admin.site.register(ContactInformation)
+admin.site.register(DirectoryInformation, DirectoryInformationAdmin)
+admin.site.register(ContactInformation, ContactInformationAdmin)
 admin.site.register(MaintenanceNotice)
